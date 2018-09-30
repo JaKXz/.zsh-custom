73a92326912b3e1902895fb3e76ce3e23878f3d3
Jason Kurian <jasonk92@gmail.com>
2018-09-29 23:52:36 -0400

    Initial commit

---
+++ git.zsh
@@ -0,0 +1,13 @@
+# Git
+alias gcb='git town prune-branches'
+alias gc='git commit'
+alias gcm='git checkout master && git stash && gup && gsa'
+
+alias st='open -a SourceTree .'
+alias gsa='git stash pop'
+
+function gri() { git rebase -i HEAD~"$1"; }            # DANGER: use with caution. pass in the number of commits to go back
+function gi() { curl -L -s https://www.gitignore.io/api/\$@ ;} # gi:Generate a gitignore for a given language/environment
+function gpu() { git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD); } # quickly create & push local branch on remote
+
+alias git-email='git config --local user.email jasonk92@gmail.com'
