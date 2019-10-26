02d0b7150d89c2ed58a88e02d8d834a407b5b442
Jason Kurian <JaKXz@users.noreply.github.com>
2019-10-25 23:42:02 -0400

    remove printing brew formulae to dropbox file
    The Brewfile has taken on this responsibility.

--- brew.zsh
+++ brew.zsh
@@ -1,5 +1,5 @@
 # Homebrew shortcuts
 brewinfo() { brew info "$1"; brew desc "$1"; }         # get all information on a brew package
 brewsearch() { brew search "$1"; brew desc -s "$1"; }  # thorough search for brew packages
-alias bu='brew update && brew upgrade && brew cleanup && brew list --versions > ~/Dropbox/Documents/miscellaneous/brew.txt'
+alias bu='brew update && brew upgrade && brew cleanup'
 alias bp='brew cleanup --prune-prefix && brew doctor'
