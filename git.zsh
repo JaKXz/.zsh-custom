bd98bc2a7c9384563737f1b89b5812f9f2b07847
Jason Kurian <JaKXz@users.noreply.github.com>
2023-04-27 23:53:13 -0400

    fix git-email alias

--- git.zsh
+++ git.zsh
@@ -9,4 +9,4 @@ function gri() { git rebase -i HEAD~"$1"; }            # DANGER: use with cautio
 function gi() { curl -L -s https://www.gitignore.io/api/\$@ ;} # gi:Generate a gitignore for a given language/environment
 function gpu() { git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD); } # quickly create & push local branch on remote
 
-alias git-email='git config --local user.email jasonk92@gmail.com'
+alias git-email='git config --local user.email JaKXz@users.noreply.github.com'
