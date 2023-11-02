56d3c84da96dc11bfc591dd8787d60631e967eaf
Jason Kurian <JaKXz@users.noreply.github.com>
2023-11-02 15:01:31 -0400

    Delete chrome.zsh

--- chrome.zsh
+++
@@ -1,6 +0,0 @@
-function chrome-app() {
-  open -na "Google Chrome" --args "--app=$1"
-}
-
-alias twitter='chrome-app 'https://mobile.twitter.com''
-# alias twitter='open -na "Google Chrome" --args "--app=https://mobile.twitter.com"'
