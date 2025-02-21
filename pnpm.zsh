9c25cd7262cb45c5f3cb16d46136bb6ebdcd1aab
Jason Kurian <JaKXz@users.noreply.github.com>
2025-02-21 16:16:48 -0500

    replace pnpm plugin with just aliases

---
+++ pnpm.zsh
@@ -0,0 +1,40 @@
+alias p='pnpm'
+
+# Dependencies
+alias pa='pnpm add'
+alias pad='pnpm add --save-dev'
+alias pap='pnpm add --save-peer'
+alias prm='pnpm remove'
+alias pin='pnpm install'
+alias pls='pnpm list'
+alias pu='pnpm update'
+alias puil='pnpm update --interactive --latest'
+
+# Global dependencies
+alias pga='pnpm add --global'
+alias pgls='pnpm list --global'
+alias pgrm='pnpm remove --global'
+alias pgu='pnpm update --global'
+
+# Run scripts
+alias pr='pnpm run'
+alias prun='pnpm run'
+alias pd='pnpm run dev'
+alias pb='pnpm run build'
+alias psv='pnpm run serve'
+alias pst='pnpm start'
+alias pt='pnpm test'
+alias ptc='pnpm test --coverage'
+alias pln='pnpm run lint'
+alias pdocs='pnpm run docs'
+alias pfmt='pnpm run format'
+alias pex='pnpm exec'
+alias pdx='pnpm dlx'
+
+# Misc
+alias pi='pnpm init'
+alias ppub='pnpm publish'
+alias pc='pnpm create'
+
+# Monorepo
+alias pf='pnpm -r --filter'
