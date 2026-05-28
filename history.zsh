3f7248d490130bdaba64ec32c3ff68b4086c5b99
Jason Kurian <jasonk92@gmail.com>
2026-05-27 23:39:35 -0400

    add utils + history

---
+++ history.zsh
@@ -0,0 +1,17 @@
+export HISTFILE=~/.zsh_history
+export HISTSIZE=10000
+export SAVEHIST=1000
+touch $HISTFILE
+
+# Search in history based on a partially written command.
+# Ensure that history is shared across sessions
+# SHARE_HISTORY: history is shared across sessions, including active sessions
+setopt SHARE_HISTORY
+
+# Bind up arrow
+bindkey '\eOA' history-beginning-search-backward
+bindkey '\e[A' history-beginning-search-backward
+
+# Bind down arrow
+bindkey '\eOB' history-beginning-search-forward
+bindkey '\e[B' history-beginning-search-forward
