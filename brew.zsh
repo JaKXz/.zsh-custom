f91b83991cdb9e7552a32338ff50562277ed921c
Jason Kurian <JaKXz@users.noreply.github.com>
2026-06-17 11:53:08 -0400

    Update brew.zsh

--- brew.zsh
+++ brew.zsh
@@ -1,5 +1,5 @@
 # Homebrew shortcuts
 brewinfo() { brew info "$1"; brew desc "$1"; }         # get all information on a brew package
 brewsearch() { brew search "$1"; brew desc -s "$1"; }  # thorough search for brew packages
-alias bu='brew update && brew upgrade && brew cleanup'
+alias bu='brew update && brew upgrade -y && brew cleanup'
 alias bp='brew cleanup --prune-prefix && brew doctor'
